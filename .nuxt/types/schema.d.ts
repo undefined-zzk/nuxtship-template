bd1abc2aac1691a382202c72815952966c3dca9e
lzh <374579253@qq.com>
2025-01-10 17:17:09 +0800

    aa

--- .nuxt/types/schema.d.ts
+++ .nuxt/types/schema.d.ts
@@ -1,34 +1,14 @@
 import { NuxtModule, RuntimeConfig } from '@nuxt/schema'
 declare module '@nuxt/schema' {
   interface NuxtOptions {
-    /**
-     * Configuration for `@nuxt/telemetry`
-     */
-    ["telemetry"]: typeof import("@nuxt/telemetry").default extends NuxtModule<infer O> ? O : Record<string, any>
   }
   interface NuxtConfig {
-    /**
-     * Configuration for `@nuxt/telemetry`
-     */
-    ["telemetry"]?: typeof import("@nuxt/telemetry").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
-    modules?: (undefined | null | false | NuxtModule<any> | string | [NuxtModule | string, Record<string, any>] | ["@nuxt/telemetry", Exclude<NuxtConfig["telemetry"], boolean>])[],
   }
 }
 declare module 'nuxt/schema' {
   interface NuxtOptions {
-    /**
-     * Configuration for `@nuxt/telemetry`
-     * @see https://www.npmjs.com/package/@nuxt/telemetry
-     */
-    ["telemetry"]: typeof import("@nuxt/telemetry").default extends NuxtModule<infer O> ? O : Record<string, any>
   }
   interface NuxtConfig {
-    /**
-     * Configuration for `@nuxt/telemetry`
-     * @see https://www.npmjs.com/package/@nuxt/telemetry
-     */
-    ["telemetry"]?: typeof import("@nuxt/telemetry").default extends NuxtModule<infer O> ? Partial<O> : Record<string, any>
-    modules?: (undefined | null | false | NuxtModule<any> | string | [NuxtModule | string, Record<string, any>] | ["@nuxt/telemetry", Exclude<NuxtConfig["telemetry"], boolean>])[],
   }
   interface RuntimeConfig {
    app: {
