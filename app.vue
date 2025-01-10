d0f3a9acc4ade087745f04f00c223cf93c580aeb
lzh <374579253@qq.com>
2025-01-10 17:27:17 +0800

    useSeoMeta

--- app.vue
+++ app.vue
@@ -1,4 +1,10 @@
 <script lang="ts" setup>
+useSeoMeta({
+  title:'nuxt 测试练习',
+  ogTitle:'nuxt 练习',
+  description:'这个一个个人学习nuxt、tailwindcss的练习网站',
+  ogDescription:'这个一个个人学习nuxt、tailwindcss的练习网站',
+})
 </script>
 <template>
   <NuxtLayout>
