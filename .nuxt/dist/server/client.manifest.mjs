cf6165e8dd0c5b97d772d1e38863dbb34dab21b1
undefined <2143511430@qq.com>
2025-01-12 18:38:52 +0800

    ff

--- .nuxt/dist/server/client.manifest.mjs
+++ .nuxt/dist/server/client.manifest.mjs
@@ -1,18 +1,1 @@
-export default {
-  "@vite/client": {
-    "prefetch": true,
-    "isEntry": true,
-    "file": "@vite/client",
-    "css": [],
-    "module": true,
-    "resourceType": "script"
-  },
-  "D:/front-end-learn/vue-nuxt-demo/node_modules/nuxt/dist/app/entry.js": {
-    "resourceType": "script",
-    "module": true,
-    "prefetch": true,
-    "preload": true,
-    "isEntry": true,
-    "file": "D:/front-end-learn/vue-nuxt-demo/node_modules/nuxt/dist/app/entry.js"
-  }
-}
+export { default } from "file:///D:/front-end-learn/vue-nuxt-demo/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
---
+++ .nuxt/dist/server/server.mjs
@@ -0,0 +1,1 @@
+export { default } from "file:///D:/front-end-learn/vue-nuxt-demo/node_modules/@nuxt/vite-builder/dist/runtime/vite-node.mjs"
