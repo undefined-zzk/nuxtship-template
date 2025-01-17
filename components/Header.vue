04bd344ce661938b9f2cde3ac3c48d5c2100a9bd
lzh <374579253@qq.com>
2025-01-17 16:15:24 +0800

    添加深浅色主题切换

--- components/Header.vue
+++ components/Header.vue
@@ -12,17 +12,20 @@ const changeLang=()=>{
     const lang=locale.value=='en'? 'zh':'en'
     setLocale(lang)
 }
+
+
 </script>
 
 <template>
-    <div class="lg:h-10 h-auto lg:flex lg:items-center px-4 lg:justify-between my-5">
+    <div class="lg:h-10 h-auto lg:flex bg-color lg:items-center px-4 lg:justify-between my-5">
         <div class="flex items-center justify-between">
             <div>
                 <nuxt-link :to="localePath('/')">
-                    <span class="font-bold text-slate-800 ">{{ $t('header.frame') }}</span><span class=" text-slate-500">{{ $t('header.projectName') }}</span>
+                    <span class="font-bold text-slate-800 text-color ">{{ $t('header.frame') }}</span><span class=" text-slate-500">{{ $t('header.projectName') }}</span>
                 </nuxt-link>
             </div>
             <span class="lg:hidden flex items-center gap-x-1 cursor-pointer">
+                <ThemeBtn/>
                 <div class="cursor-pointer" @click="changeLang">{{ locale=='en'?'中文':'English' }}</div>
                 <Icon name="uil:multiply" class="text-2xl" v-if="open" @click="open = !open"></Icon>
                 <Icon name="uil:align-justify" class="text-2xl" v-else @click="open = !open"></Icon>
@@ -30,17 +33,17 @@ const changeLang=()=>{
         </div>
         <div class="lg:flex lg:items-center hidden lg:gap-6 flex-col lg:flex-row">
             <nuxt-link :to="localePath(page.path)" v-for="(page, index) in menuItems" :key="index">
-                <span class="block h-10 leading-10 text-gray-500 hover:text-gray-900">{{ $t(`header.${page.name}`) }}</span>
+                <span class="block text-color h-10 leading-10 text-gray-500 hover:text-gray-900 dark:hover:text-slate-500">{{ $t(`header.${page.name}`) }}</span>
             </nuxt-link>
             <div class="lg:hidden flex gap-3">
                 <div class="flex-1 cursor-pointer rounded-md h-10 leading-10 text-black text-center bg-slate-300">{{ $t('btn.login') }}</div>
                 <div
-                    class="flex-1 cursor-pointer rounded-md h-10 leading-10 text-white text-center bg-black hover:bg-slate-700">{{ $t('btn.signup') }}</div>
+                    class="flex-1 cursor-pointer rounded-md h-10 leading-10 text-white text-center hover:bg-slate-700">{{ $t('btn.signup') }}</div>
             </div>
         </div>
         <div class="flex lg:hidden lg:items-center lg:gap-6 flex-col lg:flex-row" v-if="open">
             <nuxt-link :to="localePath(page.path)" v-for="(page, index) in menuItems" :key="index">
-                <span class="block h-10 leading-10 text-gray-500 hover:text-gray-900">{{ $t(`header.${page.name}`) }}</span>
+                <span class="block text-color h-10 leading-10 text-gray-500 hover:text-gray-900 dark:hover:text-slate-500">{{ $t(`header.${page.name}`) }}</span>
             </nuxt-link>
             <div class="lg:hidden flex gap-3">
                 <div class="flex-1 cursor-pointer rounded-md h-10 leading-10 text-black text-center bg-slate-300">{{ $t('btn.login') }}</div>
@@ -50,9 +53,10 @@ const changeLang=()=>{
             </div>
         </div>
         <div class="lg:flex items-center gap-2 hidden">
+            <ThemeBtn/>
             <div class="cursor-pointer" @click="changeLang">{{ locale=='en'?'中文':'English' }}</div>
             <div>{{ $t('btn.login') }}</div>
-            <div class="bg-black cursor-pointer rounded-md h-10 leading-10 px-5 text-white hover:bg-slate-700">{{ $t('btn.signup') }}
+            <div class="bg-btn cursor-pointer rounded-md h-10 leading-10 px-5 bg-text-color hover:bg-slate-700 hover:text-white">{{ $t('btn.signup') }}
             </div>
         </div>
     </div>
