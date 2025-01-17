895c17d3a249cf3fdcdb08bbd30ecbb17e4ef7e5
lzh <374579253@qq.com>
2025-01-17 11:01:39 +0800

    全部汉化

--- lang/i18n.config.ts
+++ lang/i18n.config.ts
@@ -1,12 +1,11 @@
-import zh from './zh.json'
-import en from './en.json'
+import zh from "./zh.json";
+import en from "./en.json";
 export default defineI18nConfig(() => ({
-    legacy: false,
-    locale: 'en',
-    fallbackLocale:'en',
-    messages: {
-      en,
-      zh
-    }
-  }))
-  
+  legacy: false,
+  locale: "en",
+  fallbackLocale: "en",
+  messages: {
+    en,
+    zh,
+  },
+}));
--- pages/contact.vue
+++ pages/contact.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-
+const { locale } = useI18n()
 </script>
 
 <template>
@@ -7,30 +7,29 @@
         <top zhtitle="接触" title="Contact" zhdesc="我来帮忙。" desc="I am here to help."></top>
         <div class="md:flex gap-10 md:px-32">
             <div class="flex-1 md:mb-0 mb-5">
-                <div class="text-2xl">Contact Nuxtship Creator</div>
+                <div class="text-2xl">{{ $t('contact.creator') }}</div>
                 <div class="mt-3 mb-5 text-xl text-gray-400">
-                    Have something to say? I am here to help. Fill up the form or send email.
+                    {{ $t('contact.tip') }}
                 </div>
                 <div class="flex items-center gap-4 mb-4">
                     <Icon name="uil:registered"></Icon>
-                    <span>1010 Vienna, Austria</span>
+                    <span>{{ $t('contact.one') }}</span>
                 </div>
                 <div class="flex items-center gap-4">
                     <Icon name="uil:envelope-minus"></Icon>
-                    <span>jakobaichmayr@gmail.com</span>
+                    <span>{{ $t('contact.emial') }}</span>
                 </div>
             </div>
             <div class="flex-1">
                 <form action="" class="flex flex-col items-center">
                     <input class="w-full border-2 border-[#D1D5DB] h-10 rounded pl-3" type="text" required
-                        placeholder="Full Name">
+                        :placeholder="$t('contact.form.fullname')">
                     <input class="w-full border-2 border-[#D1D5DB] h-10 rounded pl-3 my-3" type="text" required
-                        placeholder="Email Address">
+                        :placeholder="$t('contact.form.email')">
                     <textarea class="w-full h-36 border-2 border-[#D1D5DB] rounded p-3 mb-3" name="" id=""
-                        placeholder="Your Message"></textarea>
+                        :placeholder="$t('contact.form.message')"></textarea>
                     <button type="submit"
-                        class="text-white rounded-md w-full text-center py-3 bg-black hover:bg-slate-700 cursor-pointer">Send
-                        Message</button>
+                        class="text-white rounded-md w-full text-center py-3 bg-black hover:bg-slate-700 cursor-pointer">{{ $t('btn.sendmessage') }}</button>
                 </form>
             </div>
         </div>
