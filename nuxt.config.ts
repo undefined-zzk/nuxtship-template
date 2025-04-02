25b2a402e4180c440cace41757cb6e8adb791586
zzk <2143511430@qq.com>
2025-04-02 15:26:19 +0800

    deepseekApiKey

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,46 +1,46 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import AutoImport from 'unplugin-auto-import/vite'
-import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
-import Components from 'unplugin-vue-components/vite'
+import AutoImport from "unplugin-auto-import/vite";
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
 export default defineNuxtConfig({
-  compatibilityDate: '2025-01-10',
+  compatibilityDate: "2025-01-10",
   devtools: { enabled: false },
   features: {
     inlineStyles: false,
   },
   router: {
     options: {
-      scrollBehaviorType: 'smooth',
+      scrollBehaviorType: "smooth",
     },
   },
   runtimeConfig: {
-    apiSecret: 'jjfa85093fjKFJ_42)(_',
+    apiSecret: "jjfa85093fjKFJ_42)(_",
     public: {
-      baseUrl: '',
-      deepseekApiKey:process.env.DEEPSEEK_API_KEY,
+      baseUrl: "",
+      deepseekApiKey: process.env.NUXT_PUBLIC_DEEPSEEK_API_KEY,
     },
   },
   imports: {
-    dirs: ['composables/**'],
+    dirs: ["composables/**"],
   },
   modules: [
-    '@nuxt/ui',
+    "@nuxt/ui",
     [
-      '@pinia/nuxt',
+      "@pinia/nuxt",
       {
-        autoImports: ['defineStore'],
+        autoImports: ["defineStore"],
       },
     ],
-    '@nuxtjs/i18n',
-    '@vueuse/nuxt',
-    '@element-plus/nuxt',
+    "@nuxtjs/i18n",
+    "@vueuse/nuxt",
+    "@element-plus/nuxt",
   ],
   i18n: {
-    vueI18n: '~/locales/i18n.config.ts',
-    locales: ['en', 'zh'],
-    defaultLocale: 'en',
+    vueI18n: "~/locales/i18n.config.ts",
+    locales: ["en", "zh"],
+    defaultLocale: "en",
   },
-  css: ['~/assets/sass/main.scss'],
+  css: ["~/assets/sass/main.scss"],
   postcss: {
     plugins: {
       tailwindcss: {},
@@ -48,28 +48,26 @@ export default defineNuxtConfig({
     },
   },
   app: {
-    pageTransition: { name: 'page', mode: 'out-in' },
+    pageTransition: { name: "page", mode: "out-in" },
     head: {
-      title: 'Nuxtship营销网站,入门模板',
+      title: "Nuxtship营销网站,入门模板",
       meta: [
         {
-          name: 'viewport',
-          content: 'width=device-width, initial-scale=1',
-          charset: 'utf-8',
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+          charset: "utf-8",
         },
         {
-          name: 'keywords',
-          content: 'Nuxtship营销网站,入门模板',
+          name: "keywords",
+          content: "Nuxtship营销网站,入门模板",
         },
         {
-          name: 'description',
+          name: "description",
           content:
-            '使用 Nuxt 完成的营销网站,Nuxtship 是适用于初创公司、营销网站和登录页面的入门模板。使用 Nuxt 和 TailwindCSS 构建。您可以使用此入门模板快速创建任何网站。',
+            "使用 Nuxt 完成的营销网站,Nuxtship 是适用于初创公司、营销网站和登录页面的入门模板。使用 Nuxt 和 TailwindCSS 构建。您可以使用此入门模板快速创建任何网站。",
         },
       ],
-      link: [
-        { rel: 'icon', type: 'image/svg+xml', href: '/skunk.svg' },
-      ],
+      link: [{ rel: "icon", type: "image/svg+xml", href: "/skunk.svg" }],
     },
   },
   nitro: {
@@ -83,7 +81,7 @@ export default defineNuxtConfig({
       //  secure: false, // 如果是 HTTPS，可能需要关闭证书验证（开发环境）
       //  rewrite: (path:string) => path.replace(/^\/api/, '')
       // }
-   }
+    },
   },
   vite: {
     css: {
@@ -94,17 +92,17 @@ export default defineNuxtConfig({
       },
     },
     optimizeDeps: {
-      include: ['vueuc'], // 显式包含 vueuc
+      include: ["vueuc"], // 显式包含 vueuc
     },
     plugins: [AutoImport({}), Components({})],
   },
   typescript: {
     tsConfig: {
       compilerOptions: {
-        module: 'esnext', // 支持 import.meta
-        target: 'esnext', // 目标 ES 版本
-        moduleResolution: 'node',
+        module: "esnext", // 支持 import.meta
+        target: "esnext", // 目标 ES 版本
+        moduleResolution: "node",
       },
     },
   },
-})
+});
